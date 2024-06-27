ec913df1483ab5ef3f1a9b0647ff8785d176f986
Maria Fernanda <mariafernanda_fs@hotmail.com>
2024-06-26 21:20:56 -0300

    adicionando gitignore + arquivos do projeto maven webflux

---
+++ src/main/java/br/com/maferfs/desafio_api_lugares/DesafioApiLugaresApplication.java
@@ -0,0 +1,13 @@
+package br.com.maferfs.desafio_api_lugares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiLugaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiLugaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maferfs/desafio_api_lugares/DesafioApiLugaresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maferfs.desafio_api_lugares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiLugaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
